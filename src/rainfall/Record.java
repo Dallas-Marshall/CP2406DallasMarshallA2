d00b11986e0aeac3920eb86f68e88a6e6bc3ce9d
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2020-10-06 13:50:01 +1000

    Add constructor and getters

--- src/rainfall/Record.java
+++ src/rainfall/Record.java
@@ -5,5 +5,38 @@ package rainfall;
  * specific year that comes from a particular rainfall station
  */
 public class Record {
-    // TODO: add your record implementation here
-}
+    private int year;
+    private int month;
+    private double total;
+    private double min;
+    private double max;
+
+    public Record(int year, int month, double total, double min, double max) {
+        this.year = year;
+        this.month = month;
+        this.total = total;
+        this.min = min;
+        this.max = max;
+    } // end Record constructor
+
+    public int getYear() {
+        return year;
+    } // end getYear
+
+    public int getMonth() {
+        return month;
+    } // end getMonth
+
+    public double getTotal() {
+        return total;
+    } // end getTotal
+
+    public double getMin() {
+        return min;
+    } // end getMin
+
+    public double getMax() {
+        return max;
+    } // end getMax
+
+} // end class Record
