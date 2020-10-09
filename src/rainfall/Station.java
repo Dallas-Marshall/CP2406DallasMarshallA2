80cc5685e29eb7ee00414bddaea2b57168b62741
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2020-10-09 18:17:38 +1000

    Add documentation

--- src/rainfall/Station.java
+++ src/rainfall/Station.java
@@ -1,10 +1,70 @@
 package rainfall;
 
+import java.util.ArrayList;
+
 /**
- * A Station contains analysed monthly rainfall data. {@link Loader#load} is used to
- * create a Station object given a dataset station name.
+ * An object of class Station represents monthly rainfall data of a station,
+ * where each Record is a month's rainfall statistics.
  */
 public class Station {
 
-    // TODO: add your station implementation here
-}
+    // ArrayList to store Records
+    private final ArrayList<Record> rainfallRecords;
+
+    /**
+     * Create a Station object and initialise ArrayList.
+     */
+    public Station() {
+        rainfallRecords = new ArrayList<>();
+    } // end Station constructor
+
+    /**
+     * Method to add Record to Station object,
+     * all values must be checked valid by program calling the method.
+     *
+     * @param record The Record object to be added to Station.
+     */
+    public void addRecord(Record record) {
+        rainfallRecords.add(record);
+    } // end addRecord
+
+    /**
+     * Method to find and return specific Record Object.
+     *
+     * @param year  The year of the specific Record object.
+     * @param month The month of the specific Record object.
+     * @return The specific Record object if it exists in Station,
+     * otherwise returns null.
+     */
+    public Record getRecord(int year, int month) {
+        for (Record record : rainfallRecords) {
+            if (record.getYear() == year && record.getMonth() == month) {
+                return record;
+            }
+        }
+        return null;
+    } // end getRecord
+
+    /**
+     * Method to find and return Record object at position i in Station.
+     *
+     * @param i The index of the requested Record object.
+     * @return The Record object at position i in Station.
+     * @throws ArrayIndexOutOfBoundsException for values of i, that are out of bounds.
+     */
+    public Record getRecord(int i) {
+        if (i >= rainfallRecords.size()) {
+            throw new ArrayIndexOutOfBoundsException("Index %i, is Invalid");
+        } else {
+            return rainfallRecords.get(i);
+        }
+    } // end getRecord
+
+    /**
+     * @return Number of Records for Station.
+     */
+    public int numberOfRecords() {
+        return rainfallRecords.size();
+    } // end numberOfRecords
+
+} // end class Station
