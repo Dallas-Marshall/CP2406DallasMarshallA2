ec08dc3c3d322006d8593e77424481dba729b186
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2020-10-09 18:07:24 +1000

    Add documentation

--- src/rainfall/Record.java
+++ src/rainfall/Record.java
@@ -1,16 +1,25 @@
 package rainfall;
 
 /**
- * A Record contains the analysed rainfall data specific to a single month in a
- * specific year that comes from a particular rainfall station
+ * An object of class Record represents a month's rainfall data.
  */
 public class Record {
-    private int year;
-    private int month;
-    private double total;
-    private double min;
-    private double max;
+    private final int year;
+    private final int month;
+    private final double total;
+    private final double min;
+    private final double max;
 
+    /**
+     * Create a Record object and store values,
+     * all values must be checked valid by program calling the constructor.
+     *
+     * @param year  The year the rainfall data was recorded.
+     * @param month The month the rainfall data was recorded.
+     * @param total The total rainfall in mm for the specified month.
+     * @param min   The minimum rainfall recorded for the specified month.
+     * @param max   The maximum rainfall recorded for the specified month.
+     */
     public Record(int year, int month, double total, double min, double max) {
         this.year = year;
         this.month = month;
@@ -19,24 +28,46 @@ public class Record {
         this.max = max;
     } // end Record constructor
 
+    /**
+     * @return The value of year.
+     */
     public int getYear() {
         return year;
     } // end getYear
 
+    /**
+     * @return The value of month.
+     */
     public int getMonth() {
         return month;
     } // end getMonth
 
+    /**
+     * @return The value of total rainfall.
+     */
     public double getTotal() {
         return total;
     } // end getTotal
 
+    /**
+     * @return The value of minimum rainfall.
+     */
     public double getMin() {
         return min;
     } // end getMin
 
+    /**
+     * @return The value of maximum rainfall.
+     */
     public double getMax() {
         return max;
     } // end getMax
+
+    /**
+     * @return A string representation of Record in CSV format.
+     */
+    public String toString() {
+        return String.format("%d,%d,%1.2f,%1.2f,%1.2f", year, month, total, min, max);
+    } // end toString
 
 } // end class Record
